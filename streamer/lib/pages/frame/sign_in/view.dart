aa9f45aeba912cf3dfb4e62ecead8fdb543936b9
TripleJah <orangehedge@protonmail.com>
2023-03-02 15:37:48 -0600

    google sign in

--- streamer/lib/pages/frame/sign_in/view.dart
+++ streamer/lib/pages/frame/sign_in/view.dart
@@ -4,8 +4,9 @@ import 'package:get/get.dart';
 import 'package:streamer/common/values/colors.dart';
 import 'package:streamer/pages/frame/welcome/controller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:streamer/pages/frame/sign_in/controller.dart';
 
-class SignInPage extends GetView<WelcomeController> {
+class SignInPage extends GetView<SignInController> {
   const SignInPage({Key? key}) : super(key: key);
 
   Widget _buildLogo() {
@@ -23,43 +24,49 @@ class SignInPage extends GetView<WelcomeController> {
   }
 
   Widget _buildThirdPartyLogin(String loginType, String logo) {
-    return Container(
-      width: 295.w,
-      height: 44.h,
-      padding: const EdgeInsets.all(10),
-      margin: EdgeInsets.only(bottom: 15.h),
-      decoration: BoxDecoration(
-          color: AppColors.primaryBackground,
-          borderRadius: const BorderRadius.all(Radius.circular(5)),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: const Offset(0, 1))
-          ]),
-      child: Row(
-        mainAxisAlignment:
-            logo == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
-        children: [
-          logo == ''
-              ? Container()
-              : Container(
-                  padding: EdgeInsets.only(left: 40.w, right: 30.w),
-                  child: Image.asset("assets/icons/$logo.png"),
-                ),
-          Container(
-            child: Text(
-              "Sign in with $loginType",
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  color: AppColors.primaryText,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14),
-            ),
-          )
-        ],
+    return GestureDetector(
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        padding: const EdgeInsets.all(10),
+        margin: EdgeInsets.only(bottom: 15.h),
+        decoration: BoxDecoration(
+            color: AppColors.primaryBackground,
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1))
+            ]),
+        child: Row(
+          mainAxisAlignment:
+              logo == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
+          children: [
+            logo == ''
+                ? Container()
+                : Container(
+                    padding: EdgeInsets.only(left: 40.w, right: 30.w),
+                    child: Image.asset("assets/icons/$logo.png"),
+                  ),
+            Container(
+              child: Text(
+                "Sign in with $loginType",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    color: AppColors.primaryText,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14),
+              ),
+            )
+          ],
+        ),
       ),
+      onTap: () {
+        //print("third party $loginType");
+        controller.handleSignIn("google");
+      },
     );
   }
 
